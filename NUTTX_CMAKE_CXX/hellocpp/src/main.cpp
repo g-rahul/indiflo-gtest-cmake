9cbfe6eeeb4943efe8d1a83fda96d2ce17db20d0
rahulg <rahulgfalcon@gmail.com>
2024-02-09 13:11:43 +0530

    nuttx cmake libcxx

---
+++ NUTTX_CMAKE_CXX/hellocpp/src/main.cpp
@@ -0,0 +1,18 @@
+#include "HelloWorld.h"
+#include <nuttx/config.h>
+
+extern "C"
+{
+        int hellocpp_main(void)
+        {
+
+                CHelloWorld *pHelloWorld = new CHelloWorld();
+                pHelloWorld->HelloWorld();
+
+                CHelloWorld helloWorld;
+                helloWorld.HelloWorld();
+
+                delete pHelloWorld;
+                return 0;
+        }
+}
