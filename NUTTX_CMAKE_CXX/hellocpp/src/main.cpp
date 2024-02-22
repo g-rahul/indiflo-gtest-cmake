4856ca6d9180a3666f6179b8985c63a2e700a5f5
rahul <rahulgfalcon@gmail.com>
2024-02-22 22:58:43 +0530

    linking gtest cxx missing

--- NUTTX_CMAKE_CXX/hellocpp/src/main.cpp
+++ NUTTX_CMAKE_CXX/hellocpp/src/main.cpp
@@ -1,9 +1,33 @@
 #include "HelloWorld.h"
 #include <nuttx/config.h>
 
+
+#define GTEST_ENABLE
+#ifdef GTEST_ENABLE
+#include <gtest/gtest.h>
+static void gtest_init(int argc, char* argv[]) {
+
+	::testing::InitGoogleTest();
+	int result = RUN_ALL_TESTS();
+	//std::cout << "Testing programm finished. Awaiting shutdown." << std::endl;
+
+    while(1)
+    {   
+
+    	//printf("Will be linked to gtest soon\n");
+
+    	//std::cout << "Testing programm finished. Awaiting shutdown." << std::endl;
+    	//std::cout << "Testing programm finished. Awaiting shutdown." << std::endl;
+    	//HAL_Delay(1000U);
+    	//HAL_GPIO_TogglePin(KH7_USERLED_GPIO_Port,KH7_USERLED_Pin);
+    }
+}
+#endif
+
+
 extern "C"
 {
-        int hellocpp_main(void)
+        int hellocpp_main(int argc, FAR char *argv[])
         {
 
                 CHelloWorld *pHelloWorld = new CHelloWorld();
@@ -11,7 +35,13 @@ extern "C"
 
                 CHelloWorld helloWorld;
                 helloWorld.HelloWorld();
-
+	            int argcs=1;
+	            char args[]="Basic_Test";
+	            char* argvs = &args[0];
+                #ifdef GTEST_ENABLE
+                // For future episodes ;)
+	            gtest_init(argcs,&argvs);
+                #endif
                 delete pHelloWorld;
                 return 0;
         }
